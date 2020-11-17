2ea0e691051cb9104ca27b284994091e29849011
Arsh Malik <51771221+ArshMalik02@users.noreply.github.com>
2020-11-17 16:03:15 +0530

    added function rules

---
+++ rules.py
@@ -0,0 +1,20 @@
+def is_word(word_list, word):
+    '''
+    Determines if word is a valid word, ignoring
+    capitalization and punctuation
+
+    word_list (list): list of words in the dictionary.
+    word (string): a possible word.
+    
+    Returns: True if word is in word_list, False otherwise
+
+    Example:
+    >>> is_word(word_list, 'bat') returns
+    True
+    >>> is_word(word_list, 'asdf') returns
+    False
+    '''
+    
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in word_list
