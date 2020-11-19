ccb66821538d22d6bcf35b418fb24f5a6fa05750
Arsh Malik <51771221+ArshMalik02@users.noreply.github.com>
2020-11-20 01:35:40 +0530

    Implemented admin and customer auth

--- main.py
+++ main.py
@@ -7,44 +7,103 @@
 '''
 Required Libraries
 '''
-import mysql.connector
-#install using python "-m pip install mysql-connector-python"
 
 import csv
 import math
 import random
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword"
-)
-
-users = {"rian": "ilikecats", "arsh": "comeoncity"}
 #store users and password in sql instead?
 
-def createUser():
+with open('db/login/customer.csv', 'a', newline='') as cF:
+    cV = csv.writer(cF)
+    data =['Username','Password']
+    cV.writerow(data)
+
+def newCustomer():
+    
+    #Prompts new customer to create login details
+
+    with open('db/login/customer.csv','a', newline='') as cF:
+        cV = csv.writer(cF)
+        username = input('Username: ')
+        password = input('Password: ')
+        cV.writerow([username,password])
+
+def adminLogin():
     '''
-    Prompts user to create Admin or Service ID with password
-    Added to users dict if not already existing
+    Admin authentication by matching username and password from db/login/admin.csv
 
-    Example:
-    >>> Enter new username: sudo
-    >>> Enter password: asdfghjkl
+    Returns True if login successful, else it returns False
 
-    Adds {"sudo":"asdfghjkl"} to users dict
     '''
+    with open('db/login/admin.csv','r') as cF:
+        cV = csv.reader(cF)
+        flag = 0 
+        username = input('admin username: ')
+        password = input('admin password: ')
+        for i in cV:                    
+            if username==i[0] and password == i[1]:
+                flag = 1
+                break
+        if flag == 1:
+            return True
+        else:
+            return False
 
-    username = input("Enter new username: ")
-    if username not in users:
-        password = input("Enter password: ")
+def customerLogin():
+    '''
+    Customer authentication by matching username and password from db/login/admin.csv
 
-        users[username] = password
+    Returns True if login successful, else it returns False
 
+    '''
+    with open('db/login/customer.csv','r') as cF:
+        cV = csv.reader(cF)
+        flag = 0 
+        username = input('Customer username: ')
+        password = input('Customer password: ')
+        for i in cV:                    
+            if username==i[0] and password == i[1]:
+                flag = 1
+                break
+        if flag == 1:
+            return True
+        else:
+            return False
+
+
+# DRIVER CODE STARTS FROM HERE
+
+while True:
+    user = input('A:Admin, C:Customer, Q:Quit ')
+    if user.upper() == 'A':
+        
+        if adminLogin():
+            print('Login successful. Welcome!')
+            # After this admin can do stuff with stockItems.csv if entire new item added
+            # update currentStock.csv if quantity of existing item added
+        else:
+            print('Incorrect Username or Password')
+
+    elif user.upper() == 'C':
+        status = input('Login or Sign up ? ')
+        if status.upper() == 'LOGIN':
+
+            if customerLogin():
+                print('Login successful. Welcome!')
+                # After this customer needs to view all items from stockItems.csv
+                # The items chosen need to be added to db/shopping cart/cart.csv
+                # Implement changes in currentStock.csv
+            else:
+                print('Incorrect Username or Password')
+
+        elif status.upper() == 'SIGN UP':
+            newCustomer()
+        else:
+            print('Wrong option!')
+    elif user.upper()=='Q':
+        break 
+    
     else:
-        print("User already exists")
+        print('Wrong option!')      
 
-def login():
-    '''
-
-    '''
